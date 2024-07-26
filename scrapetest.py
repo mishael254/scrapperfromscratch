2791436ecb3cf5f8afdc688ebe2919dca430a0ff
mishael254 <mishaelmishantoz@gmail.com>
2024-07-26 10:31:52 +0300

    learn about handling various errors ththrown by the urlopen function ie the http error and the url error

--- scrapetest.py
+++ scrapetest.py
@@ -1,8 +1,17 @@
 from urllib.request import urlopen
-from urllib.error import HTTPError
+from urllib.error import HTTPError, URLError
 from bs4 import BeautifulSoup
 
+try:
+    html = urlopen('http://pythonscraping.com/pages/page1.html')
+except HTTPError as e:
+    print(e)
+except URLError as e:
+    print("the server could not be found\n ")
+else:
+    print("The server successfully found\n")
+    bs = BeautifulSoup(html.read(),'lxml')
+    print(bs.body)
 
-html = urlopen('http://pythonscraping.com/pages/page1.html')
-bs = BeautifulSoup(html.read(),'lxml')
-print(bs.body)
+
+
