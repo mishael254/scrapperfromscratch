b94b976834d55a603d10ea965caa722aa27de5c8
mishael254 <mishaelmishantoz@gmail.com>
2024-07-26 14:35:30 +0300

    add another excemption in the function to check for url

--- scrapetest.py
+++ scrapetest.py
@@ -28,6 +28,8 @@ def getTitle(url):
         html = urlopen(url)
     except HTTPError as e:
         return None
+    except URLError as e:
+        print("bad url!")
     try:
         bs = BeautifulSoup(html.read(), 'lxml')
         title = bs.body.h1
@@ -36,7 +38,7 @@ def getTitle(url):
     return title
 
 
-title = getTitle("https://www.pythonscraping.com/pages/page1.html")
+title = getTitle("https://www.pythoscraping.com/pages/page1.html")
 if (title == None):
     print("The title could not be found\n")
 else:
