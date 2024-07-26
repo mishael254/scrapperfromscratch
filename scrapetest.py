42ada5bd3b8fc2bd5baf672f86880abaa95bd62d
mishael254 <mishaelmishantoz@gmail.com>
2024-07-26 10:17:07 +0300

    handling excemptions

--- scrapetest.py
+++ scrapetest.py
@@ -1,7 +1,8 @@
 from urllib.request import urlopen
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
 
 html = urlopen('http://pythonscraping.com/pages/page1.html')
 bs = BeautifulSoup(html.read(),'lxml')
-print(bs)
+print(bs.body)
